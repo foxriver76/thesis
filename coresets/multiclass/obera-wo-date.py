c10beba3077fb0262364248c157dd6b070505dcf
Moritz Heusinger <moritz.heusinger@gmail.com>
2022-03-10 14:32:19 +0100

    added all obera variants separately

--- coresets/multiclass/obera-wo-date.py
+++ coresets/multiclass/obera-wo-date.py
@@ -2,8 +2,10 @@
 
 import pandas as pd
 
-data = pd.read_csv('data/obera.csv', index_col=0)
+data = pd.read_csv('../../datasets/obera.csv', index_col=0)
 
 data = data.drop(columns='DateTime')
+data = data.drop(columns='Phase_compressed')
+data = data.drop(columns='Production')
 
-data.to_csv('data/obera_pre.csv', index=False)
+data.to_csv('../../datasets/obera_high.csv', index=False)
